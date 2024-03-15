e1fc0c39a65865439391efad978b27e2c0a82b82
sathizspade26 <134524943+sathizspade26@users.noreply.github.com>
2024-03-15 11:20:08 +0530

    Add files via upload

---
+++ Kissflow design/js/main.js
@@ -0,0 +1,17 @@
+let listElements = document.querySelectorAll('.link');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        if (listElement.classList.contains('active')){
+            listElement.classList.remove('active');
+        }else{
+            listElements.forEach (listE => {
+                listE.classList.remove('active');
+            })
+            listElement.classList.toggle('active');
+        }
+    })
+});
+
+
+
